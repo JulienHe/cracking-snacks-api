ee27e8b4efe3100eb371a2ca25f5688d85a098e1
Julien Henrotte <hoshi.graphi@gmail.com>
2022-08-05 17:14:19 -0400

    Production Keys

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 module.exports = {
   upload: {
     config: {
@@ -6,6 +7,21 @@ module.exports = {
         bucketName: "cracking-snacks",
         publicFiles: true,
         uniform: true,
+        serviceAccount: {
+          type: "service_account",
+          project_id: "vocal-partition-333715",
+          private_key_id: process.env.PRIVATE_KEY_ID,
+          private_key: process.env.PRIVATE_GCP,
+          client_email:
+            "crackin-snacks@vocal-partition-333715.iam.gserviceaccount.com",
+          client_id: "100045114607376465483",
+          auth_uri: "https://accounts.google.com/o/oauth2/auth",
+          token_uri: "https://oauth2.googleapis.com/token",
+          auth_provider_x509_cert_url:
+            "https://www.googleapis.com/oauth2/v1/certs",
+          client_x509_cert_url:
+            "https://www.googleapis.com/robot/v1/metadata/x509/crackin-snacks%40vocal-partition-333715.iam.gserviceaccount.com",
+        },
         baseUrl: "https://assets.crackingsnacks.com",
         basePath: "",
       },
