5e5bf291d75b647e3aa2cb3bd81fcbb340b2b013
Julien Henrotte <hoshi.graphi@gmail.com>
2022-08-06 09:51:56 -0400

    Oups

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -1,41 +1,49 @@
-module.exports = {
-  upload: {
-    config: {
-      provider: "@strapi-community/strapi-provider-upload-google-cloud-storage",
-      providerOptions: {
-        bucketName: "cracking-snacks",
-        publicFiles: true,
-        uniform: true,
-        serviceAccount: {
-          type: "service_account",
-          project_id: "vocal-partition-333715",
-          private_key_id: secrets.PRIVATE_KEY_ID,
-          private_key: secrets.PRIVATE_GCP,
-          client_email:
-            "crackin-snacks@vocal-partition-333715.iam.gserviceaccount.com",
-          client_id: "100045114607376465483",
-          auth_uri: "https://accounts.google.com/o/oauth2/auth",
-          token_uri: "https://oauth2.googleapis.com/token",
-          auth_provider_x509_cert_url:
-            "https://www.googleapis.com/oauth2/v1/certs",
-          client_x509_cert_url:
-            "https://www.googleapis.com/robot/v1/metadata/x509/crackin-snacks%40vocal-partition-333715.iam.gserviceaccount.com",
+const doppler = require("../../../doppler-secret.js");
+
+(async () => {
+  const secrets = await doppler.getSecrets();
+  module.exports = {
+    upload: {
+      config: {
+        provider:
+          "@strapi-community/strapi-provider-upload-google-cloud-storage",
+        providerOptions: {
+          bucketName: "cracking-snacks",
+          publicFiles: true,
+          uniform: true,
+          baseUrl: "https://assets.crackingsnacks.com",
+          basePath: "",
+          serviceAccount: {
+            type: "service_account",
+            project_id: "vocal-partition-333715",
+            private_key_id: secrets.PRIVATE_KEY_ID,
+            private_key: secrets.PRIVATE_GCP,
+            client_email:
+              "crackin-snacks@vocal-partition-333715.iam.gserviceaccount.com",
+            client_id: "100045114607376465483",
+            auth_uri: "https://accounts.google.com/o/oauth2/auth",
+            token_uri: "https://oauth2.googleapis.com/token",
+            auth_provider_x509_cert_url:
+              "https://www.googleapis.com/oauth2/v1/certs",
+            client_x509_cert_url:
+              "https://www.googleapis.com/robot/v1/metadata/x509/crackin-snacks%40vocal-partition-333715.iam.gserviceaccount.com",
+          },
+          baseUrl: "https://assets.crackingsnacks.com",
+          basePath: "",
         },
-        baseUrl: "https://assets.crackingsnacks.com",
-        basePath: "",
       },
     },
-  },
-  graphql: {
-    config: {
-      endpoint: "/graphql",
-      shadowCRUD: true,
-      playgroundAlways: false,
-      depthLimit: 7,
-      amountLimit: 100,
-      apolloServer: {
-        tracing: false,
+    graphql: {
+      config: {
+        endpoint: "/graphql",
+        shadowCRUD: true,
+        playgroundAlways: false,
+        depthLimit: 7,
+        amountLimit: 100,
+        apolloServer: {
+          tracing: false,
+        },
       },
     },
-  },
-};
+  };
+})();
